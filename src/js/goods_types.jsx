2ab6bbd463d1b160c6ba43adf295c6db8c52c29c
anyingiit <anyingiit@gmail.com>
2022-07-02 18:46:37 +0800

    对秒杀页完成列表

--- src/js/goods_types.jsx
+++ src/js/goods_types.jsx
@@ -6,7 +6,7 @@ import Footer from "./commons/footer";
 import Header from "./commons/header";
 
 export default class Goods_types extends React.Component {
-  static items = (() => {
+  items = (() => {
     const result = []
     for (let i = 0; i < 29; i++) {
       result.push(
@@ -20,7 +20,7 @@ export default class Goods_types extends React.Component {
         })
     }
     return result
-  })
+  })()
 
   render() {
     return (
@@ -48,29 +48,41 @@ export default class Goods_types extends React.Component {
           </div>
           <div className="line"/>
           <div className={`cur-type w`}>
-            <img src={require("../imgs/bg_03.png")}/>
+            <img src={require("../imgs/bg_03.png")} alt={`pic`}/>
           </div>
           <div className={`goods-container w`}>
-            <div className={`item`}>
-              <div className={`detail`}>
-                <div className={`pic`}><a href={`#`}><img src={require("../upload/mobile.jpg")}/></a></div>
-                <div style={{padding: '0 14px'}}>
-                  <div className={`title`}><a>Apple苹果iPhone 6s Plus（A1699）32G 金色 移动联通电信4G手机</a></div>
-                  <div className={`price`}>
-                    <span className={`price-now`}><span className={`icon`}>¥</span>6088</span>
-                    <span className={`price-origin`}>¥6988</span>
-                  </div>
-                  <div className={`state`}>
-                    <span className={`sold-per`}>已售87%</span>
-                    <span className={`progress`}>
-                      <img src={require("../imgs/state_07.png")}/>
+            {
+              this.items.map((item, index) => {
+                return (
+                  <div className={`item`} key={index} style={(index + 1) % 4 !== 0 ? {marginRight: '13px'} : {}}>
+                    <div className={`detail`}>
+                      {/*
+                          Critical dependency: the request of a dependency is an expression
+                          require用于引入模块、 JSON、或本地文件，但是不支持直接传入变量。
+                          解决方法：通过 ES6 字符串模板 将变量转换为字符串即可。
+                      */}
+                      {/* 另记: 就算使用模板字符串向scr的require传入字符串也不行, 必须传入已经require好的资源 */}
+                      <div className={`pic`}><a href={`#`}><img src={item.pic} alt={`goods_pic`}/></a></div>
+                      <div style={{padding: '0 14px'}}>
+                        <div className={`title`}><a>{item.title}</a></div>
+                        <div className={`price`}>
+                          <span className={`price-now`}><span className={`icon`}>¥</span>{item.price}</span>
+                          <span className={`price-origin`}>¥{item.originPrice}</span>
+                        </div>
+                        <div className={`state`}>
+                          <span className={`sold-per`}>已售{item.soldPer}%</span>
+                          <span className={`progress`}>
+                      <img src={require("../imgs/state_07.png")} alt={`progress_pic`}/>
                     </span>
-                    <span>剩余<span className={`number`}>29</span>件</span>
+                          <span>剩余<span className={`number`}>{item.remain}</span>件</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={`buy-now`}><a href={`#`}>立即抢购</a></div>
                   </div>
-                </div>
-              </div>
-              <div className={`buy-now`}><a href={`#`}>立即抢购</a></div>
-            </div>
+                )
+              })
+            }
           </div>
         </div>
         <Footer/>
