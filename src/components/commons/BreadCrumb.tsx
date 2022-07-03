2468771d4f992e467c85b155ffae1c12e357d1f8
anyingiit <anyingiit@gmail.com>
2022-07-03 20:51:22 +0800

    完成面包屑组件 BreadCrumb.tsx

---
+++ src/components/commons/BreadCrumb.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../../css/base/base.css"
+import "../../css/commons/bread_crumb.css"
+
+interface Props {
+  list: {
+    value: string,
+    href: string
+  }[]
+}
+
+export default class BreadCrumb extends React.Component<Props> {
+  static propTypes = {
+    list: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.string,
+      href: PropTypes.string
+    }))
+  }
+  static defaultProps = {
+    list: [
+      {
+        value: '演示1',
+        href: '#'
+      }, {
+        value: '演示2',
+        href: '#'
+      }, {
+        value: '演示3',
+        href: '#'
+      }, {
+        value: '演示4',
+        href: '#'
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          this.props.list.map((item, index, array) => {
+            const iconRight = (() => {
+              if (index != array.length - 1) {
+                return <i className={`icon-ctrl-left`}></i>
+              }
+            })()
+            return (
+              <li key={index}>
+                <a href={`${item.href}`}><span>{item.value}</span></a>
+                {iconRight}
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+}
